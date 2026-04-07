d5c262bb090eb4a8aeb171077d132ca0ead0f2d7
Liam76900 <ljthomson@gmail.com>
2026-04-07 12:58:13 +0100

    Imported functions to main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import yfinance as yf
 import numpy as np
+import matplotlib.pyplot as plt
+
+from parameters import parameter_estimator
+from generating_shock import generating_shock
+from simulation_engine import simulate_gbm
+from risk_metrics import compute_var_es
+from option_pricing import price_european_call_mc
+from validation import theoretical_mean
 
 ticker="AAPL"
 data = yf.download(ticker, start="2020-01-01", end="2024-01-01")
 prices = data["Adj Close"]
+
+
+
--- validation.py
+++ validation.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-def theoretical(S0, mu, T):
+def theoretical_mean(S0, mu, T):
     return S0 * np.exp(mu * T)
