e5f53dd59bd79641d3ec0d12e5c96b3013147a0b
Liam76900 <ljthomson@gmail.com>
2026-04-07 13:53:55 +0100

    Improved value at risk measurement

--- risk_metrics.py
+++ risk_metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def compute_var_es(final_prices, alpha=5):
+def compute_var_es(final_prices, S0, alpha=5):
+    returns = (final_prices - S0) / S0
     value_at_risk = np.percentile(final_prices, alpha)
     expected_shortfall = final_prices[final_prices <= value_at_risk].mean()
     return value_at_risk, expected_shortfall
